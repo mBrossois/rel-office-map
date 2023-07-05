fdfd14ea23a50a02146878ebe7d58fccfea3cfe3
mBrossois <markdeneutbrossois@Marks-MacBook-Pro.local>
2023-07-05 18:44:19 +0200

    Added adjustable room url

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
+import { CoWebsite, Popup } from "@workadventure/iframe-api-typings";
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
@@ -11,6 +12,10 @@ WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
+    let gameWebsite: CoWebsite
+    let popup: any
+    console.log(WA.state.config)
+
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
         const time = today.getHours() + ":" + today.getMinutes();
@@ -18,6 +23,34 @@ WA.onInit().then(() => {
     })
 
     WA.room.area.onLeave('clock').subscribe(closePopup)
+    
+
+    WA.room.area.onEnter('UpdateGame').subscribe(async() => {
+        popup = WA.ui.openPopup('UpdateGame', 'Type the url to the website to be opened in your chat!',  [])
+        
+        WA.chat.onChatMessage((message => {
+            WA.state.urlGame = message            
+        }));
+    })
+
+    WA.room.area.onLeave('UpdateGame').subscribe(async() => {
+        if(popup) {
+            popup.close()
+        }
+    })
+
+    WA.room.area.onEnter('jitsiMeetingRoomChill').subscribe(async() => {
+        console.log(WA.state.urlGame)
+        console.log(WA.state.hasVariable('urlGame'))
+
+        gameWebsite = await WA.nav.openCoWebSite(WA.state.hasVariable('urlGame') ? WA.state.loadVariable('urlGame') as string : 'https://skribbl.io', true,  "", 50, 1, false, true)
+    })
+
+    WA.room.area.onLeave('jitsiMeetingRoomChill').subscribe(async() => {
+        if(gameWebsite) {
+            gameWebsite.close()
+        }
+    })
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
