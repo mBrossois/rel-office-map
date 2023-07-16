5a0c75ba1282f5bf0954e269b6aa6bafa52399b1
mBrossois <markdeneutbrossois@Marks-MacBook-Pro.local>
2023-07-16 13:41:35 +0200

    Removed unused code

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { CoWebsite } from "@workadventure/iframe-api-typings";
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-
-// // Calling bootstrapExtra will initiliaze all the "custom properties"  
-// bootstrapExtra();
 
 console.log('Script started successfully');
 
@@ -13,10 +9,6 @@ let currentPopup: any = undefined;
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
-
-    // let gameWebsite: CoWebsite
-    // let popup: any
 
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
@@ -26,32 +18,6 @@ WA.onInit().then(() => {
 
     WA.room.area.onLeave('clock').subscribe(closePopup)
     
-
-    // WA.room.area.onEnter('UpdateGame').subscribe(async() => {
-    //     popup = WA.ui.openPopup('UpdateGame', 'Type the url to the website to be opened in your chat!',  [])
-        
-    //     WA.chat.onChatMessage((message => {
-    //         WA.state.urlGame = message            
-    //     }));
-    // })
-
-    // WA.room.area.onLeave('UpdateGame').subscribe(async() => {
-    //     if(popup) {
-    //         popup.close()
-    //     }
-    // })
-
-    // WA.room.area.onEnter('jitsiMeetingRoomChill').subscribe(async() => {
-    //     console.log(WA.state)
-    //     gameWebsite = await WA.nav.openCoWebSite(WA.state.hasVariable('urlGame') ? WA.state.loadVariable('urlGame') as string : 'https://skribbl.io', true,  "", 50, 1, false, true)
-    // })
-
-    // WA.room.area.onLeave('jitsiMeetingRoomChill').subscribe(async() => {
-    //     if(gameWebsite) {
-    //         gameWebsite.close()
-    //     }
-    // })
-
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
